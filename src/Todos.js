e9bd6ea8caca3b7ff9bf352fc140f8d1b7aa6ee6
muhsin61 <muhsin_61_67@hotmail.com>
2020-08-23 19:52:50 +0300

    isActive

--- src/Todos.js
+++ src/Todos.js
@@ -12,18 +12,32 @@ export default function Todos(props) {
         let test = AllTodos.forEach((item) => {
             console.log(item)
             if (item !== props.todos) {
-                deneme.push(item)
                 //console.log(item)
+                deneme.push(item)
             }
         });
         setTodos([...deneme])
+    }
+    const isActive = () => {
+        console.log("test");
+        let deneme = [];
+        let test = AllTodos.forEach((item) => {
+            console.log(item);
+            let newItem = item;
+            if (newItem == props.todos) {
+                console.log(item.isActive == false);
+                item.isActive === false;
+            }
+            deneme.push(newItem);
+        });
+        setTodos([...deneme]);
     }
     if (props) {
         console.log(props.todos)
         if (props.todos.isActive == true) {
             return (
                 <div className="todo">
-                    <input type="checkbox" defaultChecked={!props.todos.isActive} onChange={(e) => console.log(e.checked + e.target.value)} />
+                    <input type="checkbox" defaultChecked={!props.todos.isActive} onClick={isActive} />
                     <h1>{props.todos.text}</h1>
                     <p onClick={handleClick}>Delete</p>
                 </div>
@@ -31,7 +45,7 @@ export default function Todos(props) {
         } else {
             return (
                 <div className="todo">
-                    <input type="checkbox" defaultChecked={props.todos.isActive} onChange={(e) => console.log(e.checked + e.target.value)} />
+                    <input type="checkbox" defaultChecked={props.todos.isActive} onClick={isActive} />
                     <h1><s>{props.todos.text}</s></h1>
                     <p onClick={handleClick}>Delete</p>
                 </div>
